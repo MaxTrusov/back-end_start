e890ba32254a558380e5406f14e48db5980da7f8
MaxTrusov <46316587+MaxTrusov@users.noreply.github.com>
2023-06-27 09:00:33 +0300

    new_commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express'
+// const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req: Request, res: Response) => {
+   let helloMessage = 'Hello Max !!!'
+   res.send(helloMessage)
+})
+
+app.listen(port, () => {
+   console.log(`Example app listening on port ${port}`)
+})
